c0bae4ce11e0d98dcefd6a5be338cf9b2da1563a
Mahmud Hasan Shanto <sshanto12344321@gmail.com>
2026-04-19 23:08:52 +0600

    Fixed forgot_pass file

--- .history/resources/views/email/forgot-password.blade_20250612034617.php
+++ .history/resources/views/email/forgot-password.blade_20250612034617.php
@@ -10,6 +10,6 @@
     <h1>Hello {{ $mailData['user']->name }}</h1>
     <p>Click below to change your password</p>
     <a href="{{ route("account.resetPassword",$mailData['token']) }}">Click Here</a>
-    <p>Thank You</p>
+    <p>Thanks</p>
 </body>
 </html>
